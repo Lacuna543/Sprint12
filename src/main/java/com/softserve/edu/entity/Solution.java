207d860b21c72a6920dad50a7d48d2001ff694a0
Kateryna Nemchenko <nemochka99@gmail.com>
2020-07-22 14:26:40 +0300

    Solution class implementation

--- src/main/java/com/softserve/edu/entity/Solution.java
+++ src/main/java/com/softserve/edu/entity/Solution.java
@@ -1,8 +1,48 @@
 package com.softserve.edu.entity;
 
-public class Solution { //Kate
+
+public class Solution {
     private int idStudent;
     private int idSprint;
     private int score;
-    // TODO
+
+    public Solution(int idStudent, int idSprint, int score) {
+        this.idStudent = idStudent;
+        this.idSprint = idSprint;
+        this.score = score;
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public int getIdSprint() {
+        return idSprint;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Solution solution = (Solution) o;
+
+        if (idStudent != solution.idStudent) return false;
+        return idSprint == solution.idSprint;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idStudent;
+        result = 31 * result + idSprint;
+        return result;
+    }
 }
